81a65b08686841674a9772e90cec640a62bed95a
Jon Hull <jonathan.hull@beefnw.com>
2020-11-27 10:53:21 -0800

    latest

--- budget_ui.py
+++ budget_ui.py
@@ -1,22 +1,27 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-window_width = 800
-window_height = 500
-widget_width = 150
-widget_height = 400
+window_width = 1600
+window_height = 600
+widget_width = 300
+widget_height = 500
 class variable_input_widget(QtWidgets.QWidget):
     def __init__(self, x, y, *args, **kwargs):
+        self.variable_input_type = "Rent"
+        self.pull_data()
         self.x = x
         self.y = y
         super().__init__(*args, **kwargs)
         self.setupUi()
-        self.variable_input_type = "Rent"
-        self.amount_spent = 950
+
+    def pull_data(self):
+        self.amount_spent = 0
         self.amount_budgeted = 950
 
     def setupUi(self):
         self.setObjectName("self")
+        self.setStyleSheet('self { border: 1px solid black; }')
+
         self.resize(widget_width, widget_height)
         self.verticalLayoutWidget = QtWidgets.QWidget(self)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(self.x, self.y, widget_width, widget_height))
@@ -36,33 +41,62 @@ class variable_input_widget(QtWidgets.QWidget):
         self.label_remaining_balance = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label_remaining_balance.setObjectName("label_remaining_balance")
         self.verticalLayout.addWidget(self.label_remaining_balance)
-        self.list_widget = QtWidgets.QListWidget(self.verticalLayoutWidget)
-        self.list_widget.setObjectName("list_widget")
-        self.verticalLayout.addWidget(self.list_widget)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+
+
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.list_layout = QtWidgets.QHBoxLayout()
+        self.list_layout.setObjectName("list_layout")
+        self.list_layout.setContentsMargins(0,0,0,0)
+        self.list_layout.setSpacing(0)
+        self.comment_list_widget = QtWidgets.QListWidget(self.verticalLayoutWidget)
+        self.comment_list_widget.setObjectName("comment_list_widget")
+        self.comment_list_widget.setFont(font)
+        self.list_layout.addWidget(self.comment_list_widget)
+        self.date_list_widget = QtWidgets.QListWidget(self.verticalLayoutWidget)
+        self.date_list_widget.setObjectName("date_list_widget")
+        self.date_list_widget.setFont(font)
+        self.list_layout.addWidget(self.date_list_widget)
+        self.amount_list_widget = QtWidgets.QListWidget(self.verticalLayoutWidget)
+        self.amount_list_widget.setObjectName("amount_list_widget")
+        self.amount_list_widget.setFont(font)
+        self.list_layout.addWidget(self.amount_list_widget)
+        self.verticalLayout.addLayout(self.list_layout)
+
+
+        self.button_layout = QtWidgets.QHBoxLayout()
+        self.button_layout.setObjectName("button_layout")
         self.button_view = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.button_view.setObjectName("button_view")
-        self.horizontalLayout.addWidget(self.button_view)
+        self.button_layout.addWidget(self.button_view)
         self.button_add = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.button_add.setObjectName("button_add")
-        self.horizontalLayout.addWidget(self.button_add)
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.button_layout.addWidget(self.button_add)
+        self.verticalLayout.addLayout(self.button_layout)
 
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def add_text(self, s):
-        item = self.list_widget.insertItem(0, s)
+    def add_text(self, c, d, a):
+        item = self.comment_list_widget.insertItem(0, c)
+        item = self.date_list_widget.insertItem(0, d)
+        item = self.amount_list_widget.insertItem(0, "${:.2f}".format(a))
+        self.amount_spent = self.amount_spent + a
+        self.retranslateUi()
 
     def retranslateUi(self):
-        #self.setWindowTitle("Variable Input Type")
         self.label_variable_input_type.setText(self.variable_input_type)
-        self.label_budgeted_amount.setText("Budgeted: {}".format(self.amount_budgeted))
-        self.label_total_spent.setText("Spent: {}".format(self.amount_spent))
-        self.label_remaining_balance.setText("Remaining {}".format(self.amount_spent - self.amount_budgeted))
+        self.label_budgeted_amount.setText("Budgeted: {:.2f}".format(self.amount_budgeted))
+        self.label_total_spent.setText("Spent: {:.2f}".format(self.amount_spent))
+        self.label_remaining_balance.setText("Remaining: {:.2f}".format(self.amount_budgeted - self.amount_spent))
         self.button_view.setText("View")
         self.button_add.setText("Add")
+
+    def add_entry(self):
+        pass
+
+    def view(self):
+        pass
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent = None):
@@ -77,16 +111,26 @@ class MainWindow(QtWidgets.QMainWindow):
         self.layout = QtWidgets.QHBoxLayout()
         self.setCentralWidget(self.window)
         self.window.setLayout(self.layout)
+        self.title = QtWidgets.QLabel(self.window)
+        self.title.setGeometry(QtCore.QRect(10, 10, 400, 40))
+        self.title.setText("Hull Family Budget")
+        font = QtGui.QFont('Cursive', 25)
+        self.title.setFont(font)
 
         self.widgets = []
         for i in range(5):
-            widget = variable_input_widget(0, 0)
+            widget = variable_input_widget(0, 60)
             self.layout.addWidget(widget)
             self.widgets.append(widget)
 
         text = ["Rent", "Groceries", "Car", "Misc", "House"]
-        for (widget, t) in zip(self.widgets, text):
-            widget.add_text(t)
+        date = ["2020-11-25", "2020-11-15", "2020-11-13", "2020-11-15", "2020-11-25"]
+        amount = [950, 223.56, 14.34, 438.99, 11.21]
+        budget = [950, 200, 50, 100, 200]
+        for (widget, t, d, a, b) in zip(self.widgets, text, date, amount, budget):
+            widget.variable_input_type = t
+            widget.amount_budgeted = b
+            widget.add_text(t, d, a)
 
 
 if __name__ == "__main__":
