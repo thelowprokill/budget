86216e5ec027dc6f4f2b574929447a36cbf52cc4
Jon Hull <jonathan.hull@beefnw.com>
2020-12-02 15:18:46 -0800

    own_file

---
+++ data.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from decimal  import Decimal
+
+class title_data:
+    def __init__(self):
+        self.title = "title"
+        self.budgeted = Decimal(0)
+        self.spent = Decimal(0)
+        self.remaining = Decimal(0)
+        self.variable_input_id = 0
+        self.month = 0
+
+class entry:
+    def __init__(self):
+        self.c = ""
+        self.d = datetime.now().date()
+        self.a = 0
