c554b0cd573a0eff144130a2fedb66828b12b192
thelowprokill <jonathan.hull@beefnw.com>
2020-12-04 19:08:54 -0800

    removed_debug_statements

--- connect.py
+++ connect.py
@@ -2,16 +2,6 @@ from mysql.connector import connect, Error, errorcode
 from decimal import Decimal
 import config_loader as cl
 
-def message(num, s):
-    print(s)
-#config = cl.config_loader(message).val
-#connection = connect(host=config.mysql_host, database=config.mysql_data, user=config.mysql_user, password=config.mysql_pass)
-
-#cursor = connection.cursor()
-#cursor.execute("select * from variable_input")
-#results = cursor.fetchall()
-#print(results)
-
 class data_handler:
     def __init__(self, config, message):
         self.message = message
